d7040d8b6a33365aeaa152ec1943d0116f8b804a
Redouane Achouri <achouri.a.r@gmail.com>
2018-12-06 22:03:15 +0100

    Added code to read DFs

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -13,5 +13,19 @@ object MainClass extends SparkMachinery {
             .csv(conf.getString("data.path") + "/user_artist_data.txt")
 
         data.show(5)
+
+
+        val rawUserArtistData = spark.read
+            .textFile(conf.getString("data.path") + "/user_artist_data.txt")
+            .repartition(4)     // Distribute the data on all 4 cores
+
+        rawUserArtistData.take(5).foreach(println)
+
+        val userArtistDf = rawUserArtistData.map(line => {
+            val Array(user, artist, _*) = line.split(' ')
+            (user.toInt, artist.toInt)
+        }).toDF("user", "artist")
+
+        userArtistDf.show()
     }
 }
