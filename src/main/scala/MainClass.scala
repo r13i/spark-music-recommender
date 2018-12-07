dd9f3175e7d830ace96b11e55665be2e09f81a4e
Redouane Achouri <achouri.a.r@gmail.com>
2018-12-07 13:06:00 +0100

    Fixing bugs. WIP

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -1,31 +1,59 @@
 
 // package com.example.music_recommender
 
+import org.apache.spark.sql.functions.{min, max}
 
 object MainClass extends SparkMachinery {
+    import spark.implicits._
+
     def main (args: Array[String]) {
-        
-        import spark.implicits._
 
-        val data = spark.read
-            .option("header", "true")
-            .option("inferSchema", "true")
-            .csv(conf.getString("data.path") + "/user_artist_data.txt")
-
-        data.show(5)
-
-
+        // DataFrame with ||User ID|Artist ID||
         val rawUserArtistData = spark.read
             .textFile(conf.getString("data.path") + "/user_artist_data.txt")
-            .repartition(4)     // Distribute the data on all 4 cores
+            .repartition(4)     // Distribute the data on all available cores
 
-        rawUserArtistData.take(5).foreach(println)
-
-        val userArtistDf = rawUserArtistData.map(line => {
+        val userArtistDf = rawUserArtistData.map { line =>
             val Array(user, artist, _*) = line.split(' ')
             (user.toInt, artist.toInt)
-        }).toDF("user", "artist")
+        }.toDF("user", "artist").cache()
 
-        userArtistDf.show()
+        userArtistDf.agg(min("user"), max("user"), min("artist"), max("artist")).show()
+
+
+        // DataFrame with ||ID|Artist||
+        val rawArtistData = spark.read.textFile(conf.getString("data.path") + "/artist_data.txt")
+        // Because there are corrupted entries in the Artist <-> ID data, we will do a `flatMap`
+        // instead of a `map` to drop the bad data
+        val artistById = rawArtistData.flatMap { line =>
+            val (id, name) = line.span(_ != '\t')
+            if (name.isEmpty) {
+                None
+            } else {
+                try {
+                    Some((id.toInt, name.trim))
+                } catch {
+                    case _: NumberFormatException => null
+                }
+            }
+        }.toDF("id", "artist")
+
+        artistById.show
+
+
+        // Mapping bad artist aliases to good ones
+        val rawArtistAlias = spark.read.textFile(conf.getString("data.path") + "/artist_alias.txt")
+        // Because there are corrupted entries in the alias data, we will do a `flatMap`
+        // instead of a `map` to drop the bad data
+        val artistAlias = rawArtistAlias.flatMap { line =>
+            val Array(artist, alias) = line.split('\t')
+            if (artist.isEmpty) {
+                None
+            } else {
+                Some((artist.toInt, alias.toInt))
+            }
+        }.collect().toMap
+
+        artistById.filter($"id" isin (1208690, 1003926)).show()
     }
 }
